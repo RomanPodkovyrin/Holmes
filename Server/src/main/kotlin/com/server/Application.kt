e873013f959d52114556c77425896fc31063096a
Roman <romanpodkovyrinr@gmail.com>
2021-11-19 18:23:16 +0000

    refactor: corenlp controller and json util

--- Server/src/main/kotlin/com/server/Application.kt
+++ Server/src/main/kotlin/com/server/Application.kt
@@ -3,9 +3,30 @@ package com.server
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.server.plugins.configureRouting
+//import io.ktor.network.tls.certificates.*
+//import java.io.File
 
 fun main() {
+//
+//    val keyStoreFile = File("build/keystore.jks")
+//    val keystore = generateCertificate(
+//        file = keyStoreFile,
+//        keyAlias = "sampleAlias",
+//        keyPassword = "Mir11Nash",
+//        jksPassword = "foobar"
+//    )
+
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+//        applicationEngineEnvironment {
+//            sslConnector(
+//                keyStore = keystore,
+//                keyAlias = "sampleAlias",
+//                keyStorePassword = { "foobar".toCharArray() },
+//                privateKeyPassword = { "foobar".toCharArray() }) {
+//                port = 8443
+//                keyStorePath = keyStoreFile
+//            }
+//        }
         configureRouting()
     }.start(wait = true)
 }
--- Server/src/main/kotlin/com/server/plugins/Routing.kt
+++ Server/src/main/kotlin/com/server/plugins/Routing.kt
@@ -1,35 +1,16 @@
 package com.server.plugins
 
-import io.ktor.routing.*
-import io.ktor.application.*
-import io.ktor.client.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.client.request.*
-import com.google.gson.Gson
 import com.google.gson.JsonObject
-import com.google.gson.JsonParser
-import io.ktor.client.engine.cio.*
-import io.ktor.client.features.*
+import com.server.controllers.sendBookToCoreNLP
+import com.server.utils.extractUsefulTags
+import io.ktor.application.*
 import io.ktor.http.*
-import io.ktor.util.pipeline.*
-import kotlinx.coroutines.Deferred
-import kotlinx.coroutines.async
+import io.ktor.request.*
+import io.ktor.response.*
+import io.ktor.routing.*
 import java.io.FileInputStream
 import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.system.exitProcess
-
-
-private val client = HttpClient(CIO) {
-    engine {
-        requestTimeout = 0 // 0 to disable, or a millisecond value to fit your needs
-    }
-    install(HttpTimeout) {
-        requestTimeoutMillis = 120000 // 2 mins
-    }
-}
-
 
 fun Application.configureRouting() {
     var coreNlpUrl = "localhost"
@@ -47,24 +28,29 @@ fun Application.configureRouting() {
     }
 
     routing {
+
+        // Root used to ping the server
         get("/") {
             log.info("'/' ping from ${call.request.local.remoteHost}")
             call.respondText("ping")
         }
 
+        // Checks if the book has already been processed
+        // TODO: IMPLEMENT
         get("/check-book/{bookName}/{bookAuthor}") {
             log.info("'/check-book' Check book called for book ${call.parameters["bookName"]} - ${call.parameters["bookAuthor"]}\"")
             call.respondText("IMPLEMENT ${call.parameters["bookName"]} and ${call.parameters["bookAuthor"]}")
         }
 
+        // Processes the book
         post("/process-book/{bookName}/{bookAuthor}") {
             log.info("'/process-book' Process book is called for book ${call.parameters["bookName"]} - ${call.parameters["bookAuthor"]}")
             val text = call.receiveText()
             log.debug("Received body of size ${text.length}")
 
             val requestContent = sendBookToCoreNLP(this, coreNlpUrl, coreNlpPort, text)
-            if (requestContent == "ERROR: CORENLP"){
-                call.response.status(HttpStatusCode(500,"Server side error" ))
+            if (requestContent == "ERROR: CORENLP") {
+                call.response.status(HttpStatusCode(500, "Server side error"))
 
                 return@post
             }
@@ -77,51 +63,3 @@ fun Application.configureRouting() {
     }
 }
 
-private fun extractUsefulTags(requestContent: String): Pair<ArrayList<JsonObject>, ArrayList<JsonObject>> {
-    val jsonObject = JsonParser.parseString(requestContent).asJsonObject
-    val person: ArrayList<JsonObject> = arrayListOf()
-    val location: ArrayList<JsonObject> = arrayListOf()
-    jsonObject.get("sentences").asJsonArray.forEach { sentence ->
-        sentence.asJsonObject.get("tokens").asJsonArray.forEach { token ->
-            val token = token.asJsonObject
-            when (token.get("ner").asString) {
-                "PERSON" -> person.add(token)
-                "LOCATION" -> location.add(token)
-            }
-        }
-    }
-    return Pair(person, location)
-}
-
-private suspend fun sendBookToCoreNLP(
-    pipelineContext: PipelineContext<Unit, ApplicationCall>,
-    coreNlpUrl: String,
-    coreNlpPort: String,
-    text: String
-): String {
-    val response: Deferred<String> = pipelineContext.async {
-        try {
-            client.post("http://$coreNlpUrl:$coreNlpPort/") {
-                timeout {
-                    requestTimeoutMillis = 120000 // 2 mins
-                }
-                val properties: Map<String, Any> =
-                    mapOf(
-                        "annotators" to "tokenize,ssplit,ner",//,parse,depparse,coref,kbp,quote,pos
-                        "outputFormat" to "json",
-
-                        )
-                parameter("properties", Gson().toJson(properties))
-                body = text
-            }
-        } catch (e: Exception) {
-            println("When calling Corenlp server :$e")
-            //TODO: fix log not working outside of a request
-//            log.error("When calling Corenlp server :$e")
-            "ERROR: CORENLP"
-        }
-    }
-
-//    log.info("Calling CoreNLP server")
-    return response.await()
-}
