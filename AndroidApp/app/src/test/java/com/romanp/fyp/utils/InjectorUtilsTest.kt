20483cfbaf2e3455cf31ee99cdc229776fe64728
Roman <romanpodkovyrinr@gmail.com>
2021-12-15 09:11:52 +0000

    refactor: remove unused imports

--- AndroidApp/app/src/test/java/com/romanp/fyp/utils/InjectorUtilsTest.kt
+++ AndroidApp/app/src/test/java/com/romanp/fyp/utils/InjectorUtilsTest.kt
@@ -1,7 +1,5 @@
 package com.romanp.fyp.utils
 
-import androidx.lifecycle.ViewModelProvider
-import com.romanp.fyp.viewmodels.MainActivityViewModel
 import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
