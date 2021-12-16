3410f4f3ace671db095041ff1f5b508645545212
Roman <romanpodkovyrinr@gmail.com>
2021-12-16 11:39:11 +0000

    refactor: remove imports and extract functions

--- AndroidApp/app/src/main/java/com/romanp/fyp/views/BookReaderActivity.kt
+++ AndroidApp/app/src/main/java/com/romanp/fyp/views/BookReaderActivity.kt
@@ -1,27 +1,21 @@
 package com.romanp.fyp.views
 
-import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.webkit.WebView
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.romanp.fyp.R
 import com.romanp.fyp.database.BookDatabaseHelper
 import com.romanp.fyp.models.book.BookInfo
 import com.romanp.fyp.models.book.Chapter
-import nl.siegmann.epublib.domain.Book as EpubBook
-import nl.siegmann.epublib.epub.EpubReader
-import opennlp.tools.tokenize.SimpleTokenizer
-import java.io.InputStream
 
 
 class BookReaderActivity : AppCompatActivity() {
     companion object {
         private const val TAG = "BookReaderActivity"
-        val tokenizer: SimpleTokenizer = SimpleTokenizer.INSTANCE
     }
 
     private var currentPage: Int = 0
@@ -33,13 +27,14 @@ class BookReaderActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         Log.i(TAG, "Opened Book Reader Activity")
         setContentView(R.layout.activity_book_reader)
-
-        Log.i(TAG, "check : ${intent.hasExtra("Book")}")
+        // TODO: implement MVVM
         val bookId = intent.getLongExtra("Book", -1) //TODO: do i need this default?
+
         val myBookInfo: BookInfo? =
-            BookDatabaseHelper(applicationContext).getBook(bookId)//intent.getSerializableExtra("Book") as? BookInfo
+            BookDatabaseHelper(applicationContext).getBook(bookId)
         if (myBookInfo == null || myBookInfo.image == -1) {
             Toast.makeText(
                 applicationContext,
@@ -62,30 +57,41 @@ class BookReaderActivity : AppCompatActivity() {
         findViewById<Button>(R.id.buttonNext).apply {
             setOnClickListener {
                 Log.i(TAG, "Next Button pressed")
-                if (currentPage >= myBookInfo.chapters.size - 1) {
-                    Log.i(TAG, "Max page reached")
-                    return@setOnClickListener
-                }
-                currentPage++
-                updatePage(chapters, currentPage)
+                nextButton(myBookInfo, chapters)
 
             }
         }
         findViewById<Button>(R.id.buttonBack).apply {
             setOnClickListener {
                 Log.i(TAG, "Back Button pressed")
-                if (currentPage <= 0) {
-                    Log.i(TAG, "Min page reached")
-                    return@setOnClickListener
-                }
-                currentPage--
-                updatePage(chapters, currentPage)
+                backButton(chapters)
 
             }
         }
     }
 
-    fun updatePage(chapters: ArrayList<Chapter>, page: Int) {
+    private fun nextButton(
+        myBookInfo: BookInfo,
+        chapters: ArrayList<Chapter>
+    ) {
+        if (currentPage >= myBookInfo.chapters.size - 1) {
+            Log.i(TAG, "Max page reached")
+            return
+        }
+        currentPage++
+        updatePage(chapters, currentPage)
+    }
+
+    private fun backButton(chapters: ArrayList<Chapter>) {
+        if (currentPage <= 0) {
+            Log.i(TAG, "Min page reached")
+            return
+        }
+        currentPage--
+        updatePage(chapters, currentPage)
+    }
+
+    private fun updatePage(chapters: ArrayList<Chapter>, page: Int) {
         webViewBookContent?.loadData(chapters[page].text, "text/html", "UTF-8")
         textView = findViewById<TextView>(R.id.textViewBookTitle).apply {
             text = "$bookTitle | ${chapters[page].chapterTitle}"
