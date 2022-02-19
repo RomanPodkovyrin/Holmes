4c98d9b8ac4187327c2f322ac43e12df33058bed
Roman Podkovyrin <35275333+RomanPodkovyrin@users.noreply.github.com>
2022-02-19 14:06:51 +0000

    Feature/fypu 98 pie chart by chapter (#42)
    * feat: display pie chart by chapter
    
    * examples
    
    * refactor: generalised graph code to add other visualisations
    
    * fix: stop page jumping when switching chapters with no data
    
    * fix: error when opening whole book plot

--- AndroidApp/app/src/main/assets/js/pieChart.js
+++ AndroidApp/app/src/main/assets/js/pieChart.js
@@ -1,59 +1,93 @@
-const DUMMY_DATA = [
-    {name: 'Chrome', value: 10},
-    {name: 'Edge', value: 1},
-    {name: 'Safari', value: 2},
-    {name: 'FireFox', value: 5}]
-//TODO: remove dummy data an replace with example data that matches the format
-// loadPieChart(DUMMY_DATA)
-function loadPieChart(dataset) {
-    var svg = d3.select("#piechart");
-    var height = 900;
-    var width = 500;
-    var textLabelSuffix = "%";
+function loadPieChartByChapter(chapter, dataset) {
+  var svg = d3.select("#piechart");
+  var textLabelSuffix = "%";
+  var data = [];
+  dataset
+    .filter(function (el) {
+      print(el.byChapterMentions[chapter].length);
+      return !el.byChapterMentions[chapter].length == 0;
+    })
+    .forEach((element) => {
+      data.push({
+        name: element.name,
+        mentions: element.byChapterMentions[chapter],
+      });
+    });
 
-    showPieChart(dataset, svg, height, width,
-        textLabelSuffix);
+  console.log("temp " + data.length + data);
+  if (data.length == 0) {
+    document.getElementById("message").innerHTML = "NO DATA";
+  } else {
+    document.getElementById("message").innerHTML = "Chapter " + (chapter + 1);
+  }
+  showPieChart(data, svg, textLabelSuffix);
 }
 
-function showPieChart(dataset, svg, height, width,
-                      textLabelSuffix) {
-    const container = d3.select('svg')
-        .classed('container', true)
-// .style('border', '1px solid red');
-    width = container.attr('width')
-    height = container.attr('height')
+function loadPieChart(dataset) {
+  var svg = d3.select("#piechart");
+  var textLabelSuffix = "%";
+  var data = [];
+  dataset
+    .filter(function (el) {
+      print(el.mentions.length);
+      return !el.mentions.length == 0;
+    })
+    .forEach((element) => {
+      data.push({
+        name: element.name,
+        mentions: element.mentions,
+      });
+    });
 
-    const radius = 400
-    const g = container.append('g')
-        .attr('transform', `translate(${width / 2}, ${height / 2})`)
+  document.getElementById("message").innerHTML = "Whole Book";
+  showPieChart(data, svg, textLabelSuffix);
+}
 
-// const colour = d3.scaleOrdinal(['red', 'blue', 'green', 'gray'])
-    const pie = d3.pie()
-        // .sort(null)
-        .value(d => d.mentions.length)
+function showPieChart(dataset, svg, textLabelSuffix) {
+  // Clear Previous graph
+  d3.selectAll("svg > *").remove();
+  // d3.select("svg").remove();
 
-    const path = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(100)
+  const container = d3.select("svg").classed("container", true);
+  // .style('border', '1px solid red');
+  var width = container.attr("width");
+  var height = container.attr("height");
 
-    const label = d3.arc()
-        .outerRadius(radius)
-        .innerRadius(radius - 110)
+  const radius = 400;
+  const g = container
+    .append("g")
+    .attr("transform", `translate(${width / 2}, ${height / 2})`);
 
-    const pies = g.selectAll('.arc')
-        .data(pie(dataset))
-        .enter()
-        .append('g')
-        .attr('class', 'arc')
+  // const colour = d3.scaleOrdinal(['red', 'blue', 'green', 'gray'])
+  const pie = d3
+    .pie()
+    // .sort(null)
+    .value((d) => d.mentions.length);
 
-    const color = d3.scaleOrdinal(d3.schemeCategory10);
-    pies.append('path')
-        .attr('d', path)
-        .attr("fill", function (d, i) {
-            return color(i);
-        })
+  const path = d3.arc().outerRadius(radius).innerRadius(100);
 
-    pies.append('text')
-        .text(d => d.data.name)
-        .attr('transform', d => `translate(${label.centroid(d)})`)
+  const label = d3
+    .arc()
+    .outerRadius(radius)
+    .innerRadius(radius - 110);
+
+  const pies = g
+    .selectAll(".arc")
+    .data(pie(dataset))
+    .enter()
+    .append("g")
+    .attr("class", "arc");
+
+  const color = d3.scaleOrdinal(d3.schemeSpectral[11]);
+  pies
+    .append("path")
+    .attr("d", path)
+    .attr("fill", function (d, i) {
+      return color(i);
+    });
+
+  pies
+    .append("text")
+    .text((d) => d.data.name + "(" + d.data.mentions.length + ")")
+    .attr("transform", (d) => `translate(${label.centroid(d)})`);
 }
--- AndroidApp/app/src/main/java/com/romanp/fyp/views/BookGraphActivity.kt
+++ AndroidApp/app/src/main/java/com/romanp/fyp/views/BookGraphActivity.kt
@@ -3,6 +3,7 @@ package com.romanp.fyp.views
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -10,26 +11,39 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.romanp.fyp.R
+import com.romanp.fyp.models.book.BookInfo
 import com.romanp.fyp.models.book.Entity
 import com.romanp.fyp.utils.InjectorUtils
 import com.romanp.fyp.viewmodels.BookGraphActivityViewModel
+import com.romanp.fyp.viewmodels.graph.GraphType
 
 
 class BookGraphActivity : AppCompatActivity() {
     companion object {
+        val gson = Gson()
         private const val TAG = "BookGraphActivity"
+        const val BOOKID_GRAPH = "ID_OF_BOOK"
+        const val GRAPH_TYPE = "GRAPH_TYPE"
     }
 
     private lateinit var viewModel: BookGraphActivityViewModel
+    private lateinit var type: GraphType
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_book_graph)
         //TODO: pass type of graph as an enum to display right graph
         val bookId = intent.getLongExtra(
-            "ID",
+            BOOKID_GRAPH,
             -1
         )
+
+        val extras = intent.extras
+        if ((extras != null) && (extras.containsKey(GRAPH_TYPE))) {
+            type = extras.getSerializable(GRAPH_TYPE) as GraphType
+        }
+
+
         initialiseViewModel(bookId)
 
         setUpWebView()
@@ -53,17 +67,27 @@ class BookGraphActivity : AppCompatActivity() {
                 view: WebView,
                 url: String
             ) {
-
-                // after the HTML page loads,
-                // load the pie chart
-                loadPieChart(viewModel.getCharacters())
+                when (type) {
+                    GraphType.PIE_CHART -> loadPieChart(
+                        viewModel.getCurrentBookInfo(),
+                        viewModel.getCharacters()
+                    )
+                }
             }
         }
 
-        // note the mapping from  file:///android_asset
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                Log.d("WebView", consoleMessage.message() + "\n" + consoleMessage.lineNumber())
+                return true
+            }
+        }
+
         webView.loadUrl(
             "file:///android_asset/" +
-                    "html/pieChart.html"
+                    when (type) {
+                        GraphType.PIE_CHART -> "html/pieChart.html"
+                    }
         )
     }
 
@@ -73,15 +97,15 @@ class BookGraphActivity : AppCompatActivity() {
     }
 
 
-    fun loadPieChart(data: ArrayList<Entity>) {
-        Log.i(TAG, "Loading Pie Chart")
-        val gson = Gson()
-        // the array as text
-        val text: String = gson.toJson(data).toString()
+    fun loadPieChart(book: BookInfo, data: ArrayList<Entity>) {
+        val chapterNumber = book.chapters.size
 
-        // pass the JSON to the JavaScript function
+        val dataJson: String = gson.toJson(data).toString()
+        val bookJson: String = gson.toJson(book).toString()
+
+        // pass the call JavaScript
         findViewById<WebView>(R.id.WebViewGraph).loadUrl(
-            "javascript:loadPieChart($text)"
+            "javascript:createButtons($bookJson,$chapterNumber, $dataJson)"
         )
     }
 }
