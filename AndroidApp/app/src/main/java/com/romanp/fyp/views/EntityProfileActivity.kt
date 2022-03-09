ba95b92687fe26f34a93dc1980c94db65a8c3ccc
Roman <romanpodkovyrinr@gmail.com>
2022-03-09 18:28:54 +0000

    refactor

--- AndroidApp/app/src/main/java/com/romanp/fyp/views/EntityProfileActivity.kt
+++ AndroidApp/app/src/main/java/com/romanp/fyp/views/EntityProfileActivity.kt
@@ -18,8 +18,13 @@ class EntityProfileActivity : AppCompatActivity() {
         const val ENTITY_TYPE = "Entity Type"
     }
 
+    private lateinit var entityName: TextView
+    private lateinit var entityNer: TextView
+    private lateinit var chapterMentionsTV: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
 
         val bookId = intent.getLongExtra(BOOK_ID, -1)
         val entityNameID = intent.getStringExtra(ENTITY_NAME)
@@ -30,9 +35,23 @@ class EntityProfileActivity : AppCompatActivity() {
             ToastUtils.toast(applicationContext, "Problem opening entity")
             return
         }
+        setContentView(R.layout.activity_entity_profile)
+        entityName = findViewById(R.id.entityName)
+        entityNer = findViewById(R.id.entityNer)
+        chapterMentionsTV = findViewById(R.id.chapterMentionsTV)
 
+        setupEntityInformationViews(bookId, entityType, entityNameID)
+        return
+    }
+
+    private fun setupEntityInformationViews(
+        bookId: Long,
+        entityType: Int,
+        entityNameID: String?
+    ) {
         val bookInfo = BookRepository.getInstance().getBookInfo(applicationContext, bookId)
 
+        // Get appropriate entity
         val entity = when (EntityType.values()[entityType]) {
             EntityType.CHARACTER -> bookInfo.characters
             EntityType.LOCATION -> bookInfo.locations
@@ -44,16 +63,15 @@ class EntityProfileActivity : AppCompatActivity() {
         }
 
         Log.i(TAG, "Character: $entity")
-        setContentView(R.layout.activity_entity_profile)
-        val entityName = findViewById<TextView>(R.id.entityName)
-        val entityNer = findViewById<TextView>(R.id.entityNer)
-        val chapterMentionsTV = findViewById<TextView>(R.id.chapterMentionsTV)
+
         entityName.text = entity.name
         entityNer.text = entity.ner
         var chapterMentionsText = ""
+        // Get and display all mentions by chapter
         entity.byChapterMentions.forEachIndexed { index, chapter ->
             chapterMentionsText += "${bookInfo.chapters[index].chapterTitle} - ${chapter.size} mentions\n"
         }
+
         chapterMentionsTV.apply {
             text = chapterMentionsText
             movementMethod = LinkMovementMethod.getInstance()
