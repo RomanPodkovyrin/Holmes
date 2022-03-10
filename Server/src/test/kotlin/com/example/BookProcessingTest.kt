97059bab45f1fd7355e9bcbdc6489314a40c611d
Roman <romanpodkovyrinr@gmail.com>
2022-03-10 15:53:43 +0000

    feat: add unit test for BookProcessing.kt

--- Server/src/test/kotlin/com/example/BookProcessingTest.kt
+++ Server/src/test/kotlin/com/example/BookProcessingTest.kt
@@ -3,22 +3,20 @@ package com.example
 import com.example.TestUtils.Companion.getFileFromPath
 import com.google.gson.Gson
 import com.server.models.BookData
-import com.server.plugins.Chapter
+import com.server.plugins.BookInfo
 import com.server.utils.extractUsefulTags
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.fail
 
 class BookProcessingTest {
     private val gson = Gson()
     private val nlpProcessedBook = getFileFromPath("testing/BookProcessingTest/alice.json")?.readText()
-    private val chapters = arrayListOf(
-        Chapter("Chapter 1", "Chapter 1 body"),
-        Chapter("Chapter 2", "Chapter 2 body"),
-        Chapter("Chapter 3", "Chapter 3 body"),
-    )
+    private val bookInfo = getFileFromPath("testing/BookProcessingTest/aliceBookInfo.json")?.readText()
+    private val expectedBookDataJson = getFileFromPath("testing/BookProcessingTest/aliceBookData.json")?.readText()
 
     @Before
     fun setUp() {
@@ -29,24 +27,39 @@ class BookProcessingTest {
     fun cleanUp() {
     }
 
-    //    @Test
-    //TODO: make them work again
+    @Test
     fun `test tag Extraction`() {
+        val bookInfo = gson.fromJson(bookInfo, BookInfo::class.java)
+        val expectedBookData = gson.fromJson(expectedBookDataJson, BookData::class.java)
 
         if (nlpProcessedBook != null) {
             val bookData = extractUsefulTags(
-                "Alice's Adventures in Wonderland",
-                "Lewis Carrol",
+                bookInfo.title,
+                bookInfo.author,
                 nlpProcessedBook,
-                chapters
+                bookInfo.chapters
             )
-            val expectedBookDataJson = getFileFromPath("testing/BookProcessingTest/aliceBookData.json")?.readText()
-            val expectedBookData = gson.fromJson(expectedBookDataJson, BookData::class.java)
+
+            assertNotNull(expectedBookData)
+            assertEquals(expectedBookData.author, bookData.author, "Author should match")
+            assertEquals(expectedBookData.title, bookData.title, "Title should match")
+            assertEquals(
+                expectedBookData.characters.size,
+                bookData.characters.size,
+                "Number of characters should match"
+            )
+            assertEquals(expectedBookData.locations.size, bookData.locations.size, "Number of locations should match")
+            assertEquals(
+                expectedBookData.characterDistanceByChapter.size,
+                bookData.characterDistanceByChapter.size,
+                "Distance by chapters size should match"
+            )
             assertEquals(expectedBookData, bookData, "Extracted Book data should match")
         } else {
             fail("FAIL: Failed to load file for testing")
         }
 
     }
+
 }
 
