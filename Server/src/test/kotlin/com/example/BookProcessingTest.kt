594577b0201de108e2a8f34771c2131f8bc04be5
Roman <romanpodkovyrinr@gmail.com>
2022-03-10 16:40:17 +0000

    test: testing text extraction between entities

--- Server/src/test/kotlin/com/example/BookProcessingTest.kt
+++ Server/src/test/kotlin/com/example/BookProcessingTest.kt
@@ -5,6 +5,7 @@ import com.google.gson.Gson
 import com.server.models.BookData
 import com.server.plugins.BookInfo
 import com.server.utils.extractUsefulTags
+import com.server.utils.getTextBetweenEntities
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -17,6 +18,17 @@ class BookProcessingTest {
     private val nlpProcessedBook = getFileFromPath("testing/BookProcessingTest/alice.json")?.readText()
     private val bookInfo = getFileFromPath("testing/BookProcessingTest/aliceBookInfo.json")?.readText()
     private val expectedBookDataJson = getFileFromPath("testing/BookProcessingTest/aliceBookData.json")?.readText()
+
+
+    private val fullText =
+        "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice “without pictures or conversations?”" +
+                "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her." +
+                "here was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, “Oh dear! Oh dear! I shall be late!” (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); " +
+                "but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge."
+
+    private val expectedExtractedText =
+        "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, “and what is the use of a book,” thought Alice “without pictures or conversations?”" +
+                "So she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit"
 
     @Before
     fun setUp() {
@@ -60,6 +72,19 @@ class BookProcessingTest {
         }
 
     }
+
+    @Test
+    fun `test getting text between two character locations`() {
+        var extractedText = getTextBetweenEntities(fullText, Pair(0, 0), Pair(0, 558))
+        assertEquals(expectedExtractedText, extractedText, "Should return correct extraction of the text")
+        extractedText = getTextBetweenEntities(fullText, Pair(0, 558), Pair(0, 0))
+        assertEquals(
+            expectedExtractedText,
+            extractedText,
+            "Should return correct extraction of the text, location order should not matter"
+        )
+    }
+
 
 }
 
